b8ae04b9c91e181b3a44fb94d3d5bdad06df90c8
Dhana <dhana_dhira@apps.ipb.ac.id>
2020-07-12 17:20:07 +0700

    Added NavigationDrawer into MyActionBar

--- MyActionBar/app/src/main/java/com/example/myactionbar/MainActivity.java
+++ MyActionBar/app/src/main/java/com/example/myactionbar/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.myactionbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button navButton = findViewById(R.id.navdrawer_button);
+        navButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        if (searchManager != null) {
+            SearchView searchView = (SearchView) (menu.findItem(R.id.search)).getActionView();
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setQueryHint(getResources().getString(R.string.search_hint));
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu1:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new MenuFragment())
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+            case R.id.menu2:
+                Intent i = new Intent(this, MenuActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return true;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.navdrawer_button:
+                Intent intent = new Intent(MainActivity.this, NavigationDrawerActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
