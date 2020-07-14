bf880dee9fbb8670b4f4334ceb719293004a44fe
Dhana <dhana_dhira@apps.ipb.ac.id>
2020-07-14 17:17:19 +0700

    Finished MyBoundService, part of MyService.

--- MyService/app/src/main/java/com/example/myservice/MainActivity.java
+++ MyService/app/src/main/java/com/example/myservice/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myservice;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 
@@ -9,10 +12,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnStartService;
-    Button btnStartIntentService;
+    Button btnStartService;         // for Simple service
+    Button btnStartIntentService;   // for Intent Service
+
+    // For BoundService
     Button btnStartBoundService;
     Button btnStopBoundService;
+    boolean mServiceBound = false;
+    MyBoundService mBoundService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnStopBoundService.setOnClickListener(this);
     }
 
+    // This variable will be used as connection of this activity and MyBoundService
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyBoundService.MyBinder myBinder = (MyBoundService.MyBinder) service;
+            myBinder.getService();
+            mServiceBound = true;
+        }
+    };
+
+    // When the button stop service pressed, this method will be used to unbind the current
+    // working bound service and also PREVENTING THE MEMORY LEAKS from bound service
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mServiceBound) {
+            unbindService(mServiceConnection);
+        }
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -43,8 +75,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startService(mStartIntentService);
                 break;
             case R.id.btn_start_bound_service:
+                Intent mBoundServiceIntent = new Intent(MainActivity.this, MyBoundService.class);
+                bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
                 break;
             case R.id.btn_stop_bound_service:
+                unbindService(mServiceConnection);
                 break;
         }
     }
--- MyService/app/src/main/java/com/example/myservice/MyBoundService.java
+++ MyService/app/src/main/java/com/example/myservice/MyBoundService.java
@@ -2,15 +2,81 @@ package com.example.myservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
+import android.os.CountDownTimer;
 import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Steps:
+ * 1. Create the Service
+ * 2. Set the variables {@link #TAG}, {@link #mBinder}, {@link #startTime}
+ * 3. Create the inner class {@link MyBinder}
+ * 4. Set up the timer (Create the timer variable) {@link #mTimer}
+ * 5. Set up the service methods and use the timer variable mTimer declared at step 4:
+ * {@link #onCreate()},
+ * {@link #onDestroy()},
+ * {@link #onBind},
+ * {@link #onRebind},
+ * {@link #onUnbind}
+ * <p>
+ * 6. Set up the ServiceConnection at main (parent) class/activity/fragment.
+ * In this context, it's the mServiceConnection in class {@link MainActivity}
+ * 7. Assign the things will be done when clicking button referring to this service in parent class
+ * 8. Set up the method onDestroy() in {@link MainActivity}
+ */
 
 public class MyBoundService extends Service {
-    public MyBoundService() {
+    private final String TAG = MyBoundService.class.getSimpleName();
+    private final MyBinder mBinder = new MyBinder();
+    private final long startTime = System.currentTimeMillis();
+    CountDownTimer mTimer = new CountDownTimer(100000, 1000) {
+        @Override
+        public void onTick(long l) {
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            Log.d(TAG, "onTick: " + elapsedTime);
+        }
+
+        @Override
+        public void onFinish() {
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.d(TAG, "onBind: ");
+        mTimer.start();
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind: ");
+        mTimer.cancel();
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Log.d(TAG, "onRebind: ");
+    }
+
+    class MyBinder extends Binder {
+        MyBoundService getService() {
+            return MyBoundService.this;
+        }
     }
 }
