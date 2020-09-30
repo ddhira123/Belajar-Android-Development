69485904cc0eb7b81c2449c63924e40b7cf03125
ddhira123 <dhana_dhira@apps.ipb.ac.id>
2020-09-30 19:32:14 +0700

    Finished MyNotesApp

---
+++ MyNotesApp/app/src/main/java/com/example/mynotesapp/db/DatabaseHelper.java
@@ -0,0 +1,51 @@
+package com.example.mynotesapp.db;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.mynotesapp.db.DatabaseContract.NoteColumns;
+
+// Class for DDL
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static String DATABASE_NAME = "dbnoteapp";
+    private static final int DATABASE_VERSION = 1;
+    private static final String SQL_CREATE_TABLE_NOTE = String.format("CREATE TABLE %s"
+                    + " (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL)",
+            DatabaseContract.TABLE_NAME,
+            NoteColumns._ID,
+            NoteColumns.TITLE,
+            NoteColumns.DESCRIPTION,
+            NoteColumns.DATE
+    );
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    public DatabaseHelper(Context context){
+        super(context,DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_TABLE_NOTE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.TABLE_NAME);
+        onCreate(db);
+    }
+}
